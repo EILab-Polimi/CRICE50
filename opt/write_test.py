48fccd489584fc29ecf82ebc4441316186cd3eff
Angelo Carlino <angelocarlino988@gmail.com>
2021-03-28 22:27:22 +0200

    adaptation completed - fixes needed

--- opt/write_test.py
+++ opt/write_test.py
@@ -1,10 +1,10 @@
 import numpy as np
-with open('./test_policy.txt', 'w+') as f:
+with open('./test_policy_ad.txt', 'w+') as f:
 	# for el in range(57*58):
 	# 	vars = np.random.rand(2)
 	# 	# print(vars)
 	# 	f.write(str(vars[0]*1.2)+"\t"+str(vars[1])+"\t")
-	for el in range(74):
+	for el in range(114):
 		# vars = np.random.rand(2)
 		# print(vars)
 		f.write(str(np.random.rand(1)[0])+"\t")
